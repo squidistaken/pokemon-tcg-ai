a7c09959ca00279edeaa2e8313b4e426af7bfaca
Matthijs <matthijs.vanderlende@gmail.com>
2026-08-16 16:37:21 +0200

    fix unit test

--- src/training/self_play.py
+++ src/training/self_play.py
@@ -605,6 +605,7 @@ def _load_snapshot(
         encoder,
         device="cpu",
         action_selection=str(
-            cfg.train.get("opponent_action_selection", "greedy") or "greedy"
+            cfg.get("train", {}).get("opponent_action_selection", "greedy")
+            or "greedy"
         ),
     )
